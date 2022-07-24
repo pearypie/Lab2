48439f5c27a91dbeaa377db9db645411cc47451f
pearypie <bosswithballs@gmail.com>
2022-07-24 21:48:30 +0700

    Widget-formet

--- week5-6-commit&push/Holoapp/holoapp/lib/Homescreen.dart
+++ week5-6-commit&push/Holoapp/holoapp/lib/Homescreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:holoapp/FoodMenu.dart';
 
+import 'MoneyBox.dart';
+
 class Homescreen extends StatefulWidget {
   const Homescreen({Key? key}) : super(key: key);
 
@@ -19,15 +21,23 @@ class _HomescreenState extends State<Homescreen> {
   int number = 0;
   Widget build(BuildContext context) {
     return Scaffold(
-        body: ListView.builder(
-            itemCount: menu.length,
-            itemBuilder: (BuildContext context, int index) {
-              FoodMenu food = menu[index];
-              return ListTile(
-                leading: Image.network(food.image),
-                title: Text("ชื่อรายการ : ${food.name}"),
-                subtitle: Text("ราคา : ${food.price}"),
-              );
-            }));
+        body: Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(children: [
+        MoneyBox("ยอดคงเหลือ", 100000, Colors.red, 100),
+        SizedBox(
+          height: 5,
+        ),
+        MoneyBox("รายรับ", 100000, Colors.orange, 100),
+        SizedBox(
+          height: 5,
+        ),
+        MoneyBox("รายจ่าย", 100000, Colors.green, 100),
+        SizedBox(
+          height: 5,
+        ),
+        MoneyBox("ค้างขำระเงิน", 100000, Colors.purple, 100),
+      ]),
+    ));
   }
 }
---
+++ week5-6-commit&push/Holoapp/holoapp/lib/MoneyBox.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class MoneyBox extends StatefulWidget {
+  final String title;
+  final double amount;
+  final Color color;
+  final double size;
+  MoneyBox(this.title, this.amount, this.color, this.size, {Key? key})
+      : super(key: key);
+
+  @override
+  State<MoneyBox> createState() => _MoneyBoxState();
+}
+
+class _MoneyBoxState extends State<MoneyBox> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: widget.color, borderRadius: BorderRadius.circular(10)),
+      height: widget.size,
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          children: [
+            Text(
+              widget.title,
+              style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Expanded(
+                child: Text(
+              widget.amount.toString(),
+              style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              textAlign: TextAlign.right,
+            ))
+          ],
+        ),
+      ),
+    );
+  }
+}
