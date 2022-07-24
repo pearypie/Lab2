1d171f651e3750b7908365ca942d33d8ac691fa5
pearypie <bosswithballs@gmail.com>
2022-07-24 19:57:13 +0700

    Event and funtion

--- week5-6-commit&push/Holoapp/holoapp/lib/Homescreen.dart
+++ week5-6-commit&push/Holoapp/holoapp/lib/Homescreen.dart
@@ -9,7 +9,36 @@ class Homescreen extends StatefulWidget {
 
 class _HomescreenState extends State<Homescreen> {
   @override
+  int number = 0;
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      floatingActionButton: Padding(
+        padding: new EdgeInsets.all(10.0),
+        child: Row(
+          children: [
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  number++;
+                });
+              },
+              child: Icon(Icons.add),
+            ),
+            FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  number--;
+                });
+              },
+              child: Icon(Icons.remove),
+            )
+          ],
+        ),
+      ),
+      body: Container(
+          child: Center(
+        child: Text(number.toString()),
+      )),
+    );
   }
 }
