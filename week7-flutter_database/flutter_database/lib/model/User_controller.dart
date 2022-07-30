cc4db86ea57fbebf531bc1f192fcbac7dd0fb98f
pearypie <bosswithballs@gmail.com>
2022-07-30 22:41:43 +0700

    Provider_create

---
+++ week7-flutter_database/flutter_database/lib/model/User_controller.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_database/model/User_datamodel.dart';
+
+class UserProvider with ChangeNotifier {
+  List<User> user = [
+    User('TestSub1', 'Datamodel', 1114755563256, 'male'),
+    User('TestSub2', 'Datamodel', 5454554512863, 'female'),
+    User('TestSub3', 'Datamodel', 4147117567415, 'male'),
+    User('TestSub4', 'Datamodel', 4545242056876, 'female')
+  ];
+
+  List<User> getdata() {
+    return user;
+  }
+
+  void addUser(User userdata) {
+    user.add(userdata);
+  }
+}
---
+++ week7-flutter_database/flutter_database/lib/model/User_datamodel.dart
@@ -0,0 +1,8 @@
+class User {
+  String name;
+  String surname;
+  int number;
+  String sex;
+
+  User(this.name, this.surname, this.number, this.sex);
+}
